50d4e5461efeab64efb165e44f9f74d156204cae
Andreas Karageorgos <info@karageorgos.me>
2025-07-11 01:26:27 +0300

    commit
    Signed-off-by: Andreas Karageorgos <info@karageorgos.me>

---
+++ captureImage.py
@@ -0,0 +1,33 @@
+import cv2
+from os import sep
+from random import randint
+from secrets import choice
+from string import ascii_letters,digits
+from time import sleep
+
+
+
+def captureImage(path,total_images=1):
+    image_names = []
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        raise Exception("Cannot open camera")
+
+    ret,frame = cap.read()
+    sleep(1)
+    for i in range(total_images):
+        if not ret:
+            raise Exception("Can't receive frame (stream end?). Exiting ...")
+        else:
+            # Save the captured image
+            image_name = path + sep + "".join([choice(ascii_letters+digits) for _ in range(randint(13,60))]) + ".jpg"
+            image_names.append(image_name)
+            cv2.imwrite(image_name, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+    return image_names
+
+
+if __name__ == "__main__":
+    captureImage("images")
---
+++ secureRandom.py
@@ -0,0 +1,40 @@
+from os import walk,sep,remove
+from captureImage import captureImage
+from secrets import choice
+from hashlib import sha512
+
+class secureRandom():
+    
+    def __init__(self,path:str):
+        self.path = path
+        self.images = []
+        
+
+    def randomInt(self):
+        self.captureImages()
+        with open( choice(self.images), "rb") as f:
+            data = f.read()
+            f.close()
+        self.deleteImages()
+        starting_image_index = data.find(b"\xFF\xDA")
+        ending_image_index = data.find(b"\xFF\xD9")
+        digest = sha512(data[starting_image_index+1:ending_image_index]).digest()
+        return int.from_bytes(digest)
+
+    def deleteImages(self):
+        for image_path in self.images:
+            remove(image_path)
+        self.images.clear()
+
+    def captureImages(self):
+        self.images = self.images + captureImage(self.path,10)
+        
+        
+            
+
+sr = secureRandom("images")
+
+number = sr.randomInt()
+
+
+print(number)
