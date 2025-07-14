da7c0a8efe3f1bc946d5cd29ef08e23659ef4613
Andreas Karageorgos <info@karageorgos.me>
2025-07-14 14:04:05 +0300

    commit
    Signed-off-by: Andreas Karageorgos <info@karageorgos.me>

--- NoiseRandom/NoiseRandom.py
+++ NoiseRandom/NoiseRandom.py
@@ -1,8 +1,32 @@
-from os import remove
-from captureImage import captureImage
+from os import remove,sep
 from secrets import choice,randbits
 from gmpy2 import is_prime as isprime
 import sys
+import cv2
+from time import sleep
+
+def captureImage(path,total_images=1,camera=0):
+    image_names = []
+    cap = cv2.VideoCapture(camera)
+
+    if not cap.isOpened():
+        raise Exception("Cannot open camera")
+
+    ret,frame = cap.read()
+    sleep((1 + 1/(randbits(6)+0.000001))) #Waits for the cam to open.
+    for i in range(total_images):
+        if not ret:
+            raise Exception("Can't receive frame (stream end?). Exiting ...")
+        else:
+            ret,frame = cap.read()
+            # Save the captured image
+            image_name = path + sep + str(i) + ".jpg"
+            image_names.append(image_name)
+            cv2.imwrite(image_name, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+    return image_names
+
 
 class NoiseRandom():
     
