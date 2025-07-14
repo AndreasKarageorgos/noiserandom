fd1aa980a014ac63ac54ea9db4e5e4b8e0c6906d
Andreas Karageorgos <info@karageorgos.me>
2025-07-14 15:35:31 +0300

    commit
    Signed-off-by: Andreas Karageorgos <info@karageorgos.me>

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme:
+    description = readme.read()
+
 setup(
     name="NoiseRandom",
+    author="Andreas Karageorgos",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         "opencv-python",
         "gmpy2"
     ],
-    license="MIT License"
+    license="MIT",
+    long_description=description,
+    long_description_content_type="text/markdown"
 )
